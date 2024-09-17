321218aeb6f4df85218f2be55027c8eecc366ca3
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2024-09-17 16:13:10 -0400

    Change redirect

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -29,7 +29,7 @@ const register = async (req, res) => {
         email
       }
     })
-    res.redirect('/login')
+    res.redirect('/')
   } catch (error) {
     console.error('Error creating user:', error)
     res.status(500).json({ message: 'Internal server error' })
