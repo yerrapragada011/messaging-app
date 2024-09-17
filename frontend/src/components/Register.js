081f27d3a253b695cdece5e38dc62461369c0422
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2024-09-17 15:15:49 -0400

    Remove proxy & add console log for api url

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -8,6 +8,7 @@ function Register() {
   const [error, setError] = useState(null)
   const navigate = useNavigate()
   const apiUrl = process.env.REACT_APP_BACKEND_API_URL
+  console.log(`API URL: ${apiUrl}`)
 
   const handleSubmit = async (e) => {
     e.preventDefault()
